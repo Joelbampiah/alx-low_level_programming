b5ffc9c51138a907dc4cdbd4a2e9e05b32f97891
Joelbampiah <joelbampiah@gmailcom>
2023-02-17 09:52:52 -0800

    Prints lower cases alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - printing lower case alphabets
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char abcd;
+
+	for (abcd = 'a'; abcd <= 'z'; abcd++)
+	{
+		putchar(abcd);
+	}
+	putchar('\n');
+	return (0)
+}
