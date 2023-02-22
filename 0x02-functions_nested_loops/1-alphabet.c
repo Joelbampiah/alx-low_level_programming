a6f8c7f02d14af92ea4f9963603cf1855e2dec0a
Joelbampiah <joelbampiah@gmailcom>
2023-02-21 17:21:11 -0800

    Printing lower cases

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	char abcd;
+
+	for (abcd = 'a'; abcd <= 'z'; abcd++)
+	{
+		_putchar(abcd);
+	}
+	_putchar('\n');
+	return (0);
+}
