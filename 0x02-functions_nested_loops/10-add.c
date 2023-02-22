01b6ebceef5a2f7392c79a7b847329bd3b2cffcc
Joelbampiah <joelbampiah@gmailcom>
2023-02-21 20:21:33 -0800

    Addition is sweet

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Adds two inputted numbers
+ * @n1: input number 1
+ * @n2: input number 2
+ * Return: The results
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
