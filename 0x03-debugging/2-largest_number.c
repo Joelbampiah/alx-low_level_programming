27c472bf12daa0ab291f48af3eb96de9c7965100
Joelbampiah <joelbampiah@gmailcom>
2023-02-23 13:59:33 -0800

    Returns largest number of 3 inputted numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	if (a > b && c > b)
-	{
-		largest = c;
-	}
-	if (b > c && c > a)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
+	}
+	else
+	{
+		largest = c;
 	}
 
 	return (largest);
