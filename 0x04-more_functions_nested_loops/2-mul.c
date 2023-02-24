70e75f581adc3ec5c2990b88fc96f2d749e07e19
Joelbampiah <joelbampiah@gmailcom>
2023-02-23 16:48:57 -0800

    Malutiples two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiples two give numbers
+ * @a: the first input
+ * @b: the second input
+ * Return: product of two inputs
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
